fe1f4dad805c336aa7a7a3c0e8a800be166797bf
Manuel Ruiz <manuel.ruiz@rte-france.com>
2021-08-10 10:01:18 +0200

    -- update
    Former-commit-id: f4ce2624953aa83a120e3bead7a84e32f64f5149 [formerly e88bc778905044ca0cf3c833f71e7fe382c7e067] [formerly 03690ebcfdd2e67744c813ab725d9e2ce8d9d1a1 [formerly 03690ebcfdd2e67744c813ab725d9e2ce8d9d1a1 [formerly d364a141a879bdc01a1c6421af082be22ddaa9a6]]]
    Former-commit-id: c2ca06a89b4ef69cd946f6638a3b1a67e35fd5af [formerly 56e8de1e002bbf53a07725f7c5f917faf41d190b]
    Former-commit-id: 23b9a6dc5341eb74e9bafab981a3d26a67decf53
    Former-commit-id: 8fe91297113f2458bf8555f60d956827c3c9af6b

---
+++ ProductionUnits.jl
@@ -0,0 +1,86 @@
+module ProductionUnits
+    using Base: String
+    using Printf
+    using Base:Tuple
+
+    using ..SCOPF
+    
+    export write_uncertainties;
+    export read_uncertainties;
+    export extract_uncertainties;
+
+    function read_uncertainties(file_path::String)
+        result = Dict{Tuple{String,String,Int64,Int64},Float64}()
+        open(file_path) do file
+            for ln in eachline(file)
+                if ln[1] != '#'
+                    buffer = split_with_space(ln)
+                    scenario = buffer[1]
+                    name = buffer[2]
+                    ts = parse(Int64, buffer[3])
+                    ech = parse(Int64, buffer[4])
+                    value = parse(Float64, buffer[5])
+                    push!(result, (scenario, name, ts, ech) => value)
+                end
+            end
+        end        
+        return result
+    end
+
+    function write_uncertainties(data::Dict{Tuple{String,String,Int64,Int64},Float64}, file_path::String)
+    open(file_path, "w") do file
+        for kvp in collect(data)
+            write(file, @sprintf("%s %s %d %d %.6E\n", kvp[1][1], kvp[1][2], kvp[1][3], kvp[1][4], kvp[2]))
+            end
+        end        
+    end
+
+    function extract_uncertainties(amplTxt, name::String, id_name::Int64, id_value::Int64, SCENARIO, TS, ECH)
+        println(SCENARIO)
+        result = Dict{Tuple{String,String,Int64,Int64},Float64}()
+        for kvp in amplTxt[name].data
+            name = kvp[id_name]
+            value=parse(Float64,kvp[id_value])
+            for scenario in SCENARIO, ts in TS, ech in ECH
+                push!(result, (scenario, name, ts, ech)=>value)
+            end
+        end
+        return result
+    end
+
+    function extract_uncertainties(amplTxt, SCENARIO, TS, ECH)
+
+        gen = extract_uncertainties(amplTxt, "generators", 20, 22, SCENARIO, TS, ECH)
+        load = extract_uncertainties(amplTxt, "loads", 9, 11, SCENARIO, TS, ECH)
+        merge!(gen, load)
+        return gen
+    end
+
+    mutable struct Unit
+        name::String
+        type::String
+        startup_cost::Float64
+        activation_cost::Float64
+        variable_cost::Float64
+        reserve_hausse::Float64
+        reserve_baisse::Float64
+
+        imposable::Bool
+    end
+
+    mutable struct Load
+        name::String
+    end
+
+    # to be renamed
+    mutable struct Problem
+        units::Dict{String, Unit}
+        loads::Dict{String, Load}
+        uncertainties::Dict{Tuple{String,String,Int64,Int64},Float64}
+        network::SCOPF.Network
+    end
+
+    function get_uncertainty(problem::Problem, scenario::String, name::String, ts::Int64, ech::Int64)
+        return problem.uncertainties[scenarion, name, ts, ech];
+    end
+end
