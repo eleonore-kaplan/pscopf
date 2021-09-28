55b7ad449f851cc8f52a9106b50c851eb1c79b23
Manuel Ruiz <manuel.ruiz@rte-france.com>
2021-09-28 12:02:13 +0200

    --
    Former-commit-id: b3938b3469b51b11ad39d05733a49484dcca2cf4 [formerly 6ef86e4ade175905381f289dd35e83cdc8395bbb] [formerly 3a49d97eeaabd9095afec32778bc69db35b610fc [formerly 3a49d97eeaabd9095afec32778bc69db35b610fc [formerly fbb86d6b6da1e53eba2383b6d6b72e1b3944d830]]]
    Former-commit-id: 185944e51eabeed1b7dd461c1e25ce5e38a94140 [formerly fb451252222d7a63498947e543707066ffc1630f]
    Former-commit-id: 04215ac686fbb2e67f74496bf6c0c7a59ed04e64
    Former-commit-id: 91f6500c030b20066f463595b689f6ea57cdf102

--- main_ptdf.jl
+++ main_ptdf.jl
@@ -9,12 +9,12 @@ include(joinpath(root_path, "FakeData.jl"));
 
 
 include(joinpath(root_path, "AmplTxt.jl"));
+using Printf
 # Compute ptdf and export them as node branch value file
 
 test_name = "5buses"
 
 amplTxt = AmplTxt.read(test_name);
-
 
 network = SCOPF.Network(amplTxt);
 
@@ -22,3 +22,32 @@ ref_bus = 1;
 B = SCOPF.get_B(network, 1e-6);
 Binv = SCOPF.get_B_inv(B, ref_bus);
 PTDF = SCOPF.get_PTDF(network, Binv, ref_bus);
+
+n = length(network.bus_to_i);
+m = length(network.branches);
+
+PTDF_TRIMMER = 1e-6;
+
+println(PTDF);
+open("ptdf.txt", "w") do file     
+    ref_name =  @sprintf("\"%s\"", network.buses[ref_bus].name)
+    write(file, @sprintf("%20s %20s\n", "REF_BUS", ref_name))
+    for branch_id in 1:m
+        for bus_id in 1:n
+            branch_name =  @sprintf("\"%s\"", network.branches[branch_id].name)
+            bus_name =  @sprintf("\"%s\"", network.buses[bus_id].name)
+            if abs(PTDF[branch_id,bus_id])>PTDF_TRIMMER
+                @printf("%10s %10s %6d %6d %15.6E\n", branch_name, bus_name, branch_id, bus_id, PTDF[branch_id,bus_id])
+                write(file, @sprintf("%20s %20s %20.6E\n", branch_name, bus_name,PTDF[branch_id,bus_id]))
+            end
+        end
+    end
+end
+# buses = amplTxt["buses"];
+# for bus in buses.data
+#     num = parse(Int, bus[2]);
+#     numCC = parse(Int, bus[4]);
+#     if numCC == 0
+#         println(bus[11])
+#     end
+# end
