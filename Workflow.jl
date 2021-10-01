eedafa3e925e7ebc99d965d60b10cf21c1af9c1f
Manuel Ruiz <manuel.ruiz@rte-france.com>
2021-10-01 16:53:59 +0200

    -- clean up of the end of week
    Former-commit-id: 47ba27e172e6da50b393e237ed26ad217056c813

--- Workflow.jl
+++ Workflow.jl
@@ -1,32 +1,100 @@
-module Workflow
-    
+module Workflow    
     export Launcher;
-    export add_uncertainties;
-
-
     using ..AmplTxt;
-    using ..ProductionUnits;
     using Dates: Date, DateTime;
-
+    U_NAME=1;
+    U_SCENARIO=2;
+    U_H=3;
+    U_ECH=4;
+    K_LIMITABLE="Limitable"
+    K_IMPOSABLE="Imposable"
     mutable struct Launcher
+        # amplTxt description of the network
         ampltxt;
-
-        uncertainties::Dict{ Tuple{String, String, DateTime, DateTime}, Float64};
-        certainties::Dict{Tuple{String, String, DateTime}, Float64};
-        
+        # uncertainties, name-scenario-h-ech->value
+        uncertainties::Dict{Tuple{String,String,DateTime,DateTime},Float64};
+        # name->minP-maxP-startCost-propCost
+        units::Dict{String, Vector{Float64}};
+        # gen->type-bus
+        gen_type_bus::Dict{String, Vector{String}};
+        # line-bus->ptdf
+        ptdf::Dict{Tuple{String,String}, Float64};
     end
 
-    function Launcher(dir_path::String)
-        u = Dict{Tuple{String, String, DateTime, DateTime}, Float64}()
-        c=  Dict{Tuple{String, String, DateTime}, Float64}()
-        return Launcher(AmplTxt.read(dir_path),u, c)
+    function read_ptdf(dir_path::String)
+        result = Dict{Tuple{String,String}, Float64}();
+        open(joinpath(dir_path, "pscopf_ptdf.txt"), "r") do file
+            for ln in eachline(file)
+                # don't read commentted line 
+                if ln[1] != '#'
+                    buffer = AmplTxt.split_with_space(ln)
+                    result[buffer[1], buffer[2]] = parse(Float64, buffer[3])
+                end
+            end
+        end
+        return result
+    end
+
+    function read_gen_type_bus(dir_path::String)
+        result = Dict{String, Vector{String}}();
+        open(joinpath(dir_path, "pscopf_gen_type_bus.txt"), "r") do file
+            for ln in eachline(file)
+                # don't read commentted line 
+                if ln[1] != '#'
+                    buffer = AmplTxt.split_with_space(ln)
+                    result[buffer[1]] = buffer[2:3]
+                end
+            end
+        end
+        return result
+    end
+
+    function read_units(dir_path::String)
+        result = Dict{String, Vector{Float64}}();
+        open(joinpath(dir_path, "pscopf_units.txt"), "r") do file
+            for ln in eachline(file)
+                # don't read commentted line 
+                if ln[1] != '#'
+                    buffer = AmplTxt.split_with_space(ln)
+                    result[buffer[1]] = (parse.(Float64, buffer[3:6]))
+                end
+            end
+        end
+        return result
+    end
+    
+    function read_uncertainties(dir_path::String)
+        result = Dict{Tuple{String,String,DateTime,DateTime},Float64}()
+        open(joinpath(dir_path, "pscopf_uncertainties.txt"), "r") do file
+            for ln in eachline(file)
+                # don't read commentted line 
+                if ln[1] != '#'
+                    buffer = AmplTxt.split_with_space(ln)
+                    name = buffer[1]
+                    h = DateTime(buffer[2])
+                    ech = DateTime(buffer[3])
+                    scenario = buffer[4]
+                    value = parse(Float64, buffer[5])
+                    result[name, scenario, h, ech] = value
+                end
+            end
+        end
+        return result
+    end
+
+    function Launcher(dir_path::String)        
+        return Launcher(AmplTxt.read(dir_path),read_uncertainties(dir_path), read_units(dir_path), read_gen_type_bus(dir_path), read_ptdf(dir_path))
     end
 
     function read_ampl_txt(launcher::Launcher, dir_path::String)
         launcher.ampltxt = AmplTxt.read(dir_path)
     end
-    
+        
     function add_uncertainties(launcher::Launcher, name::String, bus_name::String, ech::DateTime, ts::DateTime, value)
         launcher.uncertainties[name, bus_name, ech, ts] = value
     end
+
+    
+    
+    include("ModelBuilder.jl");
 end
