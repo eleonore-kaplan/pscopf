0a9d56723e813760dcfb4f3356b5312339a08ffe
Manuel Ruiz <manuel.ruiz@rte-france.com>
2021-10-05 13:45:36 +0200

    --
    Former-commit-id: 56a6d84047342279dcd120d0c98e8b7997b48e99 [formerly 59d0220a599121c7479f8a7d6d39d005742913f9] [formerly f62e85b5c097ab0d2471189bbc2217e68b46a24f [formerly 20966c47b4ed7221882848389edb1113c15363ea]]
    Former-commit-id: 7427c5f5e6052b856403bc5dbab3ec550b57ab7f [formerly 34a7a012c8003b7714205da512536eb274b61f6f]
    Former-commit-id: 21a2bc772d79e24aa001ea1f0661bb4c523fff68
    Former-commit-id: a2077b8ff993672a3ea970bd40d1c784c8283a90

--- main_workflow.jl
+++ main_workflow.jl
@@ -8,59 +8,18 @@ test_name = "5buses_wind"
 dir_path = joinpath(root_path, test_name);
 
 launcher = Workflow.Launcher(dir_path);
-##############################################################
-### optimisation modelling sets
-##############################################################
-ECH,TS, S, NAMES = Workflow.get_ech_ts_s_name(launcher);
-BUSES =  Workflow.get_bus(launcher, NAMES);
-units_by_kind = Workflow.get_units_by_kind(launcher);
-units_by_bus =  Workflow.get_units_by_bus(launcher, BUSES);
-netloads = Dict([(bus, ts, s) => launcher.uncertainties[bus, s, ts, ech] for bus in BUSES, ts in TS, s in S])
-println("Number of scenario ", length(S));
-println("Number of time step is ", length(TS));
-println(ECH)
+
+ech=DateTime("2015-01-01T09:00:00")
 K_IMPOSABLE = Workflow.K_IMPOSABLE;
 K_LIMITABLE = Workflow.K_LIMITABLE;
+model, p_lim, p_imposable= Workflow.sc_opf(launcher, ech);
 
-##############################################################
-# to be in a function ...
-##############################################################
-using JuMP;
-using Dates: DateTime;
-using Printf;
-model = Model();
 
-# p_limitable[ts, s]  = min(p0[ts, s], pMax[ts])
-p_limitable = Dict{Tuple{String, DateTime, String}, VariableRef}()
-for kvp in units_by_kind[K_LIMITABLE], ts in TS, s in S
-    name =  @sprintf("p_limitable[%s,%s,%s]", kvp[1], ts, s);
-    p_limitable[kvp[1], ts, s] = @variable(model, base_name=name);
-end
 
-p_imposable = Dict{Tuple{String, DateTime, String}, VariableRef}()
-for kvp in units_by_kind[K_IMPOSABLE], ts in TS, s in S
-    name =  @sprintf("p_imposable[%s,%s,%s]", kvp[1], ts, s);
-    p_imposable[kvp[1], ts, s] = @variable(model, base_name=name);
-end
+# using Clp;
 
-# println(units_by_kind)
-# println(p_imposable)
+# # set_optimizer(model, Xpress.Optimizer);
+# set_optimizer(model, Clp.Optimizer);
 
-eod_expr = Dict([(bus, ts, s)=>AffExpr(0) for bus in BUSES, ts in TS, s in S]);
-ech=DateTime("2015-01-01T09:00:00")
-for bus in BUSES, ts in TS, s in S
-    for gen in units_by_bus[K_IMPOSABLE][bus]
-        eod_expr[bus, ts, s] += p_imposable[gen, ts, s]
-    end
-    for gen in units_by_bus[K_LIMITABLE][bus]
-        eod_expr[bus, ts, s] += p_limitable[gen, ts, s]
-    end
-end
-# println(launcher.uncertainties)
-for bus in BUSES, ts in TS, s in S
-    @constraint(model, eod_expr[bus, ts, s] == netloads[bus, ts, s])
-end
-# println(units_by_bus)
-# println(eod_expr)
+# optimize!(model);
 
-println(model)
