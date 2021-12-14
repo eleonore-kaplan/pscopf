d188a9f0d77c14c1d70e7829adc7eb393314edda
Aziz JEGHAM <aziz.jegham@eurodecision.com>
2021-12-14 16:45:36 +0100

    [src] example to launch on multiple dates
    Former-commit-id: e0d18ee2f678ff75e600d16a916aba23cd8ba49a [formerly a0d8d12f6182759e43ad6d83f0c3dbe77a05b139] [formerly 0ffb019792306d9f975b1f85e693c3db68c5210f [formerly 663ea3abc9021548187814b2b2b10db32a2226c5]]
    Former-commit-id: 83b13d85e17f7394adac4ec6b35f788e3c196bad [formerly cf4ca4ec7a176a09b304b2e354848f094ea8be65]
    Former-commit-id: 773deb99231f8d3a8d045a93d25a5fb3c6ce304a
    Former-commit-id: ae915311b13b893a09bd2e125aad5082366fe0ed

---
+++ main_multiple_ech.jl
@@ -0,0 +1,28 @@
+
+
+using Dates: Date, DateTime;
+
+
+#root_path = raw"D:\AppliRTE\repo\scopf-quanti";
+root_path = @__DIR__;
+push!(LOAD_PATH, root_path);
+cd(root_path);
+include(joinpath(root_path, "AmplTxt.jl"));
+include(joinpath(root_path, "Workflow.jl"));
+
+
+test_name = length(ARGS) > 0 ? ARGS[1] : "data/dmo/2buses/2ech";
+println("test case : ", test_name)
+dir_path = joinpath(root_path, test_name);
+
+launcher = Workflow.Launcher(dir_path);
+
+launcher.NO_LIMITABLE = false;
+launcher.NO_IMPOSABLE = false;
+launcher.NO_LIMITATION = false;
+p_res = 0;
+p_res_min = -p_res;
+p_res_max = p_res;
+results = Workflow.run(launcher, p_res_min, p_res_max);
+
+println(launcher.previsions)
