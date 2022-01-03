e9950f68512ae6d64832242bcda814df015a8d67
Aziz JEGHAM <aziz.jegham@eurodecision.com>
2022-01-03 09:15:14 +0100

    write solver log to file

--- WorkflowModeler.jl
+++ WorkflowModeler.jl
@@ -32,6 +32,27 @@ println("optimizer: ", OPTIMIZER)
 using Dates
 import Statistics
 
+"""
+    redirect_to_file(f::Function, file_p::String, mode_p="w")
+
+Execute function `f` while redirecting C and Julia level stdout to the file file_p.
+Note that `file_p` is open in write mode by default.
+
+# Arguments
+- `f::Function` : the function to execute
+- `file_p::String` : name of the file to print to
+- `mode_p` : open mode of the file (defaults to "w")
+"""
+function redirect_to_file(f::Function, file_p::String, mode_p="w")
+    open(file_p, mode_p) do file_l
+        Base.Libc.flush_cstdio()
+        redirect_stdout(file_l) do
+            f()
+            Base.Libc.flush_cstdio()
+        end
+    end
+end
+
 function get_bus(launcher::Launcher, names::Set{String})
     result = Set{String}();
     for name in names
@@ -503,7 +524,10 @@ function sc_opf(launcher::Launcher, ech::DateTime, p_res_min, p_res_max)
     write_to_file(model, model_file_l)
 
     println("Call optimizer")
-    optimize!(model);
+    solver_logfile_l = joinpath(launcher.dirpath, @sprintf("model_%s.log", ech))
+    redirect_to_file(solver_logfile_l) do
+        optimize!(model)
+    end
 
     if termination_status(model) == INFEASIBLE
         error("Model is infeasible")
