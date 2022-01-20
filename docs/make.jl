ae69abe81778a49daa1d8a9c2ad985c77f4869f2
Aziz JEGHAM <aziz.jegham@eurodecision.com>
2022-01-20 19:09:59 +0100

    [doc] blocs et responsabilités
    Former-commit-id: b1b69af0c2427fb53135ebc10417f2d4e7d5ccdc

--- docs/make.jl
+++ docs/make.jl
@@ -10,14 +10,33 @@ format = Documenter.HTML(
 
 pages = Any[
             "Home" => "index.md",
+
+            "Descriptions" => Any[
+                "Introduction" => "0_descriptions/1_introduction.md",
+                "Glossaire" => "0_descriptions/2_glossaire.md",
+                "Architecture" => "0_descriptions/3_architecture.md",
+               ],
+
+           "Modèles" => Any[
+                "Modèles du TSO" => Any[
+                    "Général" => Any[],
+                    "Mode 1" => Any[
+                        "Problem Description" => "1_models/1_pscopf_model/1_mode_1/1_problem.md",
+                        "Variables" => "1_models/1_pscopf_model/1_mode_1/2_variables.md",
+                        "Constraints" => "1_models/1_pscopf_model/1_mode_1/3_constraints.md",
+                        "Objective" => "1_models/1_pscopf_model/1_mode_1/4_objective.md",
+                        ],
+                    ],
+
+                "Modèles du marché" => Any[
+                    "Général" => Any[],
+                    "Mode 1" => Any[],
+                    ],
+
+                ],
+
             "Library" => Any[
                 "Dummy" => "lib/dummy.md",
-                ],
-            "Model" => Any[
-                "Problem Description" => "model/1_problem.md",
-                "Variables" => "model/2_variables.md",
-                "Constraints" => "model/3_constraints.md",
-                "Objective" => "model/4_objective.md",
                 ],
 ]
 
