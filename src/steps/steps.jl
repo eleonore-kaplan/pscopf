1af21326845dcb85a44c396d6b375f5686c00cd3
Aziz JEGHAM <aziz.jegham@eurodecision.com>
2022-01-27 19:13:27 +0100

    mode 2 et mode 3 first launch traces
    Former-commit-id: 7922e9fe25e2afb875afb90090499db4b13e5e35

--- src/steps/steps.jl
+++ src/steps/steps.jl
@@ -7,24 +7,90 @@
 
 struct OptimResult end
 
+
 ################################################################################
 ####       TSO
 ################################################################################
 abstract type  AbstractTSO <: AbstractRunnable  end
 
-struct TSOMode1 <: AbstractTSO
+"""
+utilisé dans les trois modes
+Prend des décisions en se référençant à une situation équilibrée par le marché
+N'utilise pas la reserve ?
+"""
+struct TSOOutFO <: AbstractTSO
 end
-function run(step::TSOMode1, context::PSCOPFContext)
-    println("TSOMode1 à l'échéance ", get_current_ech(context))
+function run(step::TSOOutFO, context::PSCOPFContext)
+    println("TSOOutFO à l'échéance ", get_current_ech(context))
     println("\tJe me référencie au précédent planning du marché pour les arrets/démarrage et l'estimation des couts : ", get_last_market_planning(context))
-    println("\tJe me référencie au précédent planning du TSO pour les arrets/démarrage : ", get_last_tso_planning(context))
+    println("\tJe me référencie à mon précédent planning du TSO pour les arrets/démarrage : ", get_last_tso_planning(context))
     #D'ou la nécessité du bloc orange qui va fournir un état réel du réseau sur lequel se basera le TSO (mais pas le marché hors fenêtre opérationnelle)
     return #result
 end
 function update!(context::PSCOPFContext, result)
     println("\tJe mets à jour le planning tso: ", get_last_tso_planning(context),
-            " en me basant sur les résultats d'optimisation.", #step.result
-            " et je ne touche pas au planning du marché")
+            " en me basant sur les résultats d'optimisation.")
+    println("\tet je ne touche pas au planning du marché")
+end
+
+"""
+utilisé pour le mode 3:
+Prend des décisions fermes vu que c'est la dernière execution du TSO
+Décide de la reserve
+"""
+struct TSOAtFO <: AbstractTSO
+end
+function run(step::TSOAtFO, context::PSCOPFContext)
+    println("TSOAtFO à l'échéance ", get_current_ech(context))
+    println("\tJe me référencie au précédent planning du marché pour les arrets/démarrage et l'estimation des couts : ", get_last_market_planning(context))
+    println("\tJe me référencie à mon précédent planning du TSO pour les arrets/démarrage : ", get_last_tso_planning(context))
+    println("\tC'est le dernier lancement du tso => le planning TSO que je fournie doit etre ferme")
+    return #result
+end
+function update!(context::PSCOPFContext, result)
+    println("\tJe mets à jour le planning tso: ", get_last_tso_planning(context),
+            " en me basant sur les résultats d'optimisation.\n")
+    println("\tJe ne touche pas au planning du marché (ou si?)")
+end
+
+"""
+utilisé pour le mode 1:
+Prend des incertitudes non équilibrées (mode 1 => plus de marché dans la FO)
+Décide de la reserve
+"""
+struct TSOInFO <: AbstractTSO
+end
+function run(step::TSOInFO, context::PSCOPFContext)
+    println("TSOInFO à l'échéance ", get_current_ech(context))
+    println("\tJe me référencie au planning du marché du début de la FO pour les arrets/démarrage et l'estimation des couts : ", get_last_market_planning(context))
+    println("\tJe me référencie à mon précédent planning du TSO pour les arrets/démarrage : ", get_last_tso_planning(context))
+    return #result
+end
+function update!(context::PSCOPFContext, result)
+    println("\tJe mets à jour le planning tso: ", get_last_tso_planning(context),
+            " en me basant sur les résultats d'optimisation.\n")
+    println("\tJe ne touche pas au planning du marché : C'est le même planning qui me servira de référence")
+end
+
+"""
+utilisé pour le mode 2:
+Prend des incertitudes pas forcément équilibrées
+Décide de la reserve
+Simule un marché d'équilibrage
+"""
+struct TSOBiLevel <: AbstractTSO
+end
+function run(step::TSOBiLevel, context::PSCOPFContext)
+    println("TSOBiLevel à l'échéance ", get_current_ech(context))
+    println("\tJe simule un marché d'équilibrage pour le pas suivant")
+    println("\tJe me référencie au planning du marché du début de la FO pour les arrets/démarrage et l'estimation des couts ?")
+    println("\tJe me référencie à mon précédent planning du TSO pour les arrets/démarrage ?")
+    return #result
+end
+function update!(context::PSCOPFContext, result)
+    println("\tJe mets à jour le planning tso: ", get_last_tso_planning(context),
+            " en me basant sur les résultats d'optimisation.\n")
+    println("\tJe ne touche pas au planning du marché?")
 end
 
 ################################################################################
@@ -32,31 +98,61 @@ end
 ################################################################################
 abstract type  AbstractMarket <: AbstractRunnable  end
 
-struct MarketMode1OutFO <: AbstractMarket
+"""
+utilisé pour les trois modes :
+Ne regarde pas le planning du TSO
+"""
+struct MarketOutFO <: AbstractMarket
 end
-function run(step::MarketMode1OutFO, context::PSCOPFContext)
-    println("MarketMode1OutFO à l'échéance ", get_current_ech(context))
+function run(step::MarketOutFO, context::PSCOPFContext)
+    println("MarketOutFO à l'échéance ", get_current_ech(context))
     println("\tJe me base sur le précédent planning du marché pour les arrets/démarrage des unités : ", get_last_market_planning(context))
     println("\tJe ne regarde pas le planning du TSO.")
     return #result
 end
 function update!(context::PSCOPFContext, result)
     println("\tJe mets à jour ce même planning marché: ", get_last_market_planning(context),
-            " en me basant sur les résultats d'optimisation.", #step.result
+            " en me basant sur les résultats d'optimisation.",
             " et je ne touche pas au planning TSO.")
 end
 
-struct MarketMode1InFO <: AbstractMarket
+"""
+utilisé pour le mode 1:
+Dans le mode 1, le marché ne s'écecutera plus dans la FO => besoin de décisions fermes
+"""
+struct MarketAtFO <: AbstractMarket
 end
-function run(step::MarketMode1InFO, context::PSCOPFContext)
-    println("MarketMode1InFO à l'échéance ", get_current_ech(context))
-    println("\tJe me base sur le précédent planning du TSO et non pas du marché pour les arrets/démarrage des unités : ", get_last_tso_planning(context))
+function run(step::MarketAtFO, context::PSCOPFContext)
+    println("MarketAtFO à l'échéance ", get_current_ech(context))
+    println("\tJe me base sur le précédent planning du marché pour les arrets/démarrage des unités : ", get_last_market_planning(context))
+    println("\tJe ne regarde pas le planning du TSO.")
+    println("\tC'est le dernier lancement du marché => je prends des décision fermes.")
     return #result
 end
 function update!(context::PSCOPFContext, result)
     println("\tJe mets à jour le planning du marché: ", get_last_market_planning(context),
             " en me basant sur les résultats d'optimisation.", #step.result
-            " et je ne touche pas au planning du TSO (ou si?).")
+            " et je ne touche pas au planning du TSO")
+end
+
+"""
+utilisé pour les modes 2 et 3
+Dans le mode 2 : je considère le planning du TSO
+Dans le mode 3 : je considère le planning du marché
+"""
+struct MarketInFO <: AbstractMarket
+end
+function run(step::MarketInFO, context::PSCOPFContext)
+    println("MarketInFO à l'échéance ", get_current_ech(context))
+    println("\tJe me base sur le dernier planning disponible (marché ou TSO) pour les arrets/démarrage des unités") #besoin de récupérer le dernier planning
+    println("\tJe ne regarde pas le planning du TSO.")
+    println("\tC'est le dernier lancement du marché => je prends des décision fermes.")
+    return #result
+end
+function update!(context::PSCOPFContext, result)
+    println("\tJe mets à jour le planning du marché: ", get_last_market_planning(context),
+            " en me basant sur les résultats d'optimisation.", #step.result
+            " et je ne touche pas au planning du TSO")
 end
 
 ################################################################################
--- test/test_wip/test_seq_launch.jl
+++ test/test_wip/test_seq_launch.jl
@@ -6,9 +6,36 @@ using DataStructures
 @testset verbose=true "test_sequence_launch" begin
 
     @testset "execution_mode_1" begin
+        println("\n\n\n")
         grid = PSCOPF.Grid()
         TS = PSCOPF.create_target_timepoints(DateTime("2015-01-01T11:00:00"))
         mode = PSCOPF.PSCOPF_MODE_1
+        ECH = PSCOPF.generate_ech(grid, TS, mode)
+
+        sequence = PSCOPF.generate_sequence(grid, TS, ECH, mode)
+
+        exec_context = PSCOPF.PSCOPFContext(PSCOPF.Grid(), TS, ECH, mode, PSCOPF.Planning("TSO"), PSCOPF.Planning("Market"))
+        PSCOPF.run!(exec_context, sequence)
+    end
+
+    @testset "execution_mode_2" begin
+        println("\n\n\n")
+        grid = PSCOPF.Grid()
+        TS = PSCOPF.create_target_timepoints(DateTime("2015-01-01T11:00:00"))
+        mode = PSCOPF.PSCOPF_MODE_2
+        ECH = PSCOPF.generate_ech(grid, TS, mode)
+
+        sequence = PSCOPF.generate_sequence(grid, TS, ECH, mode)
+
+        exec_context = PSCOPF.PSCOPFContext(PSCOPF.Grid(), TS, ECH, mode, PSCOPF.Planning("TSO"), PSCOPF.Planning("Market"))
+        PSCOPF.run!(exec_context, sequence)
+    end
+
+    @testset "execution_mode_3" begin
+        println("\n\n\n")
+        grid = PSCOPF.Grid()
+        TS = PSCOPF.create_target_timepoints(DateTime("2015-01-01T11:00:00"))
+        mode = PSCOPF.PSCOPF_MODE_3
         ECH = PSCOPF.generate_ech(grid, TS, mode)
 
         sequence = PSCOPF.generate_sequence(grid, TS, ECH, mode)
