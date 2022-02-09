94ee85132d6f6bfcc787595c6025fa5fbaf5fa9b
Aziz JEGHAM <aziz.jegham@eurodecision.com>
2022-02-09 17:12:12 +0100

    init context.horizon from sequence otherwise there can be a non-coherence between ECH in context and ECH in sequence
    Former-commit-id: 063aca49c136a846cd75b859c0f8c3300298023e [formerly 52bcb2fe1cd2aab3f82366275af59588b6daf8a2] [formerly ec21024a3df860b7ecac509fa8de73496f0a1c7a [formerly 332a5989f2aaf1b9a27b655b91be92e2d1ac636b]]
    Former-commit-id: 3b2ac8b611f8454455fd35579bce59890cd0a521 [formerly f67de1eb0098985f3a50ec81cc9d75b0a36a1019]
    Former-commit-id: 73f8e580b319058daee036a977f22ca8cef7b5c2
    Former-commit-id: 11e9bfc02ac526943a70e88a781fa6c0d31c4bcd

--- src/context.jl
+++ src/context.jl
@@ -5,7 +5,6 @@ using ..Networks
 mutable struct PSCOPFContext <: AbstractContext
     network::Networks.Network
     target_timepoints::Vector{Dates.DateTime}
-    horizon_timepoints::Vector{Dates.DateTime}
     management_mode::ManagementMode
 
     #FIXME : which gen is on ? Question : need info by TS ?
@@ -18,21 +17,63 @@ mutable struct PSCOPFContext <: AbstractContext
     assessment_uncertainties
 
     schedule_history::Vector{Schedule}
+    #Imposition
+    # ts,gen,s
+    # SortedDict{Dates.DateTime, SortedDict{String, SortedDict{String, Float64}} }
+    #Limitation : because the schedule is not enough to know the limit
+    # ts,gen
+    # SortedDict{Dates.DateTime, SortedDict{String, Float64} }
     #flows ?
-    current_ech::Dates.DateTime
+
+    horizon_timepoints::Vector{Dates.DateTime}
+    current_ech::Union{Dates.DateTime,Nothing}
 end
 
-function PSCOPFContext(network::Networks.Network, target_timepoints::Vector{Dates.DateTime}, horizon_timepoints::Vector{Dates.DateTime},
+function PSCOPFContext(network::Networks.Network, target_timepoints::Vector{Dates.DateTime},
                     management_mode::ManagementMode,
                     generators_initial_state::SortedDict{String,GeneratorState}=SortedDict{String,GeneratorState}(),
                     uncertainties::Uncertainties=Uncertainties(),
                     assessment_uncertainties=nothing,
+                    horizon_timepoints=Vector{Dates.DateTime}(),
                     )
-    return PSCOPFContext(network, target_timepoints, horizon_timepoints, management_mode,
+    return PSCOPFContext(network, target_timepoints, management_mode,
                         generators_initial_state,
                         uncertainties, assessment_uncertainties,
                         Vector{Schedule}(),
-                        horizon_timepoints[1])
+                        horizon_timepoints,
+                        nothing)
+end
+
+function set_horizon_timepoints!(context_p::PSCOPFContext, horizons::Vector{Dates.DateTime})
+    context_p.horizon_timepoints = horizons
+end
+
+function get_network(context::PSCOPFContext)
+    return context.network
+end
+
+function get_target_timepoints(context::PSCOPFContext)
+    return context.target_timepoints
+end
+
+function get_horizon_timepoints(context::PSCOPFContext)
+    return context.horizon_timepoints
+end
+
+function get_management_mode(context::PSCOPFContext)
+    return context.management_mode
+end
+
+function get_generators_initial_state(context::PSCOPFContext)
+    return context.generators_initial_state
+end
+
+function get_uncertainties(context::PSCOPFContext)
+    return context.uncertainties
+end
+
+function get_assessment_uncertainties(context::PSCOPFContext)
+    return context.assessment_uncertainties
 end
 
 function set_current_ech!(context_p::PSCOPFContext, ech::Dates.DateTime)
@@ -41,6 +82,16 @@ end
 
 function get_current_ech(context_p::PSCOPFContext)
     return context_p.current_ech
+end
+
+function get_next_ech(context_p::PSCOPFContext)
+    ech = get_current_ech(context_p)
+    next_ech_index = findfirst(x->x>ech, get_horizon_timepoints(context_p))
+    if isnothing(next_ech_index)
+        return nothing
+    else
+        return get_horizon_timepoints(context_p)[next_ech_index]
+    end
 end
 
 function safeget_last_schedule(context_p::PSCOPFContext)
