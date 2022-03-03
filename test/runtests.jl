4d311382e4205883b5649669f2e6937dcf6dacd6
Aziz JEGHAM <aziz.jegham@eurodecision.com>
2022-03-03 18:48:31 +0100

    update

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,21 +5,6 @@ import Logging
 
 using PSCOPF
 
-# root_path = dirname(@__DIR__);
-# push!(LOAD_PATH, root_path);
-# cd(root_path);
-# include(joinpath(root_path, "test", "TestHelpers.jl"));
-# include(joinpath(root_path, "AmplTxt.jl"));
-# include(joinpath(root_path, "Workflow.jl"));
-
-# Logging.with_logger(Logging.NullLogger()) do
-#     @testset verbose=true "PSCOPF_TESTS" begin
-#         include("test_dmo_levers.jl")
-#         include("test_feasibility_slacks.jl")
-#         include("test_multiple_ts.jl")
-#     end
-# end
-
 #TODO : redirect logs
 @testset verbose=true "PSCOPF_TESTS" begin
     include("test_wip/test_modes.jl")
