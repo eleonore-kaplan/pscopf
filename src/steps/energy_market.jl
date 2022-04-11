ad1b89c069da8c8734c437c5df5a8e2516e57fa4
Aziz Jegham <aziz.jegham@eurodecision.com>
2022-04-11 13:49:31 +0200

    Balance market

--- src/steps/energy_market.jl
+++ src/steps/energy_market.jl
@@ -8,12 +8,20 @@ using Printf
     configs = EnergyMarketConfigs()
 end
 
-function run(runnable::EnergyMarket,
+@with_kw mutable struct BalanceMarket <: AbstractMarket
+    configs = EnergyMarketConfigs(problem_name = "BalanceMarket",
+                                CONSIDER_TSOACTIONS_LIMITATIONS=true,
+                                CONSIDER_TSOACTIONS_IMPOSITIONS=true,
+                                CONSIDER_TSOACTIONS_COMMITMENTS=true,
+                                REF_SCHEDULE_TYPE=PSCOPF.TSO())
+end
+
+function run(runnable::Union{EnergyMarket,BalanceMarket},
             ech::Dates.DateTime, firmness, TS::Vector{Dates.DateTime},
             context::AbstractContext)
     fo_start_time = TS[1] - get_fo_length(get_management_mode(context))
     if fo_start_time <= ech
-        msg = @sprintf("invalid step at ech=%s : EnergyMarket needs to be launched before FO start (ie %s)", ech, fo_start_time)
+        msg = @sprintf("invalid step at ech=%s : step needs to be launched before FO start (ie %s)", ech, fo_start_time)
         throw( error(msg) )
     end
 
@@ -48,7 +56,7 @@ end
 function update_market_schedule!(context::AbstractContext, ech,
                                 result::EnergyMarketModel,
                                 firmness,
-                                runnable::EnergyMarket)
+                                runnable::Union{EnergyMarket,BalanceMarket})
     market_schedule = get_market_schedule(context)
     market_schedule.decider_type = DeciderType(runnable)
     market_schedule.decision_time = ech
