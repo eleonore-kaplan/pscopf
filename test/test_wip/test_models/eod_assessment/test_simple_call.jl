b9b0e84e8b62d0a5467f96696b78f556feedf378
Aziz Jegham <aziz.jegham@eurodecision.com>
2022-04-13 09:57:24 +0200

    allow extra constraints on uncertainties
    Former-commit-id: aed134e201f70cf483bd2dc65a2ef6ab2c9f8de7

--- test/test_wip/test_models/eod_assessment/test_simple_call.jl
+++ test/test_wip/test_models/eod_assessment/test_simple_call.jl
@@ -58,7 +58,23 @@ using Printf
     ech = DateTime("2015-01-01T07:00:00")
     next_ech = DateTime("2015-01-01T07:30:00")
 
-
+    #=
+                    bus 1                   bus 2
+                            |                      |
+        (limitable) wind_1_1|       "1_2"          |
+        Pmin=0, Pmax=200    |                      |
+        Csta=0, Cprop=1     |                      |
+        [50-90]             |----------------------|
+                            |         35           |
+                            |                      |
+        (imposable) prod_1_1|                      |(imposable) prod_2_1
+        Pmin=20, Pmax=200   |                      | Pmin=20, Pmax=200
+        Csta=10k, Cprop=10  |                      | Csta=50k, Cprop=50
+        [0-0]               |                      | [20-200]
+                            |                      |
+               load(bus_1)  |                      |load(bus_2)
+        [30-50]             |                      | [20-70]
+    =#
     @testset verbose=true "test_calling_with_run" begin
         limit_1 = 75.
         impositions_1 = (0., 0.)
@@ -73,6 +89,23 @@ using Printf
         @test PSCOPF.is_validated(result)
     end
 
+    #=
+                    bus 1                   bus 2
+                            |                      |
+        (limitable) wind_1_1|       "1_2"          |
+        Pmin=0, Pmax=200    |                      |
+        Csta=0, Cprop=1     |                      |
+        [50-90]             |----------------------|
+                            |         35           |
+                            |                      |
+        (imposable) prod_1_1|                      |(imposable) prod_2_1
+        Pmin=20, Pmax=200   |                      | Pmin=20, Pmax=200
+        Csta=10k, Cprop=10  |                      | Csta=50k, Cprop=50
+        [0-0]               |                      | [20-60]
+                            |                      |
+               load(bus_1)  |                      |load(bus_2)
+        [30-50]             |                      | [20-70]
+    =#
     @testset verbose=true "test_calling_with_run" begin
         limit_1 = 75.
         impositions_1 = (0., 0.)
