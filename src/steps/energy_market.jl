6ca72516ee64cdca4af29723b35b3b593b159b13
Aziz Jegham <aziz.jegham@eurodecision.com>
2022-05-31 14:39:44 +0200

    remove TSOActions::commitments make tso_schedule the default reference in the Market steps gratis starts are deduced from the reference schedule
    Former-commit-id: c48ffa22b932ca9fa9186149b81395d7573e1c69 [formerly 92bf76044c2d4e6befb5f9ebb3582cf754c42422] [formerly 95600311a6e36e1ae2e47a63337ea3f19106faba [formerly 80e13034875439add2c63eb4e18a5e0af0891375]]
    Former-commit-id: 29f903a8989ab43e08ba1b15a6b22acca2de2bb2 [formerly e52c34220a4b8737034469408b3a5270c34e5ca4]
    Former-commit-id: 679d24bf4599d5140b178fa29cbf878fe9f8ad60
    Former-commit-id: 105fbe169d0633babb384460c5b65a4108ba8287

---
+++ src/steps/energy_market.jl
@@ -0,0 +1,93 @@
+using .Networks
+
+using Dates
+using JuMP
+using Printf
+
+@with_kw mutable struct EnergyMarket <: AbstractMarket
+    configs = EnergyMarketConfigs()
+end
+
+function run(runnable::Union{EnergyMarket,BalanceMarket},
+            ech::Dates.DateTime, firmness, TS::Vector{Dates.DateTime},
+            context::AbstractContext)
+    fo_start_time = TS[1] - get_fo_length(get_management_mode(context))
+    if (fo_start_time <= ech) && isa(runnable, EnergyMarket)
+        msg = @sprintf("invalid %s step at ech=%s : EnergyMarket step needs to be launched before FO start (ie %s)",
+                        typeof(EnergyMarket), ech, fo_start_time)
+        throw( error(msg) )
+    end
+
+    problem_name_l = @sprintf("%s_%s", typeof(runnable), ech)
+
+    gratis_starts = Set{Tuple{String,Dates.DateTime}}()
+    if runnable.configs.CONSIDER_GRATIS_STARTS
+        gratis_starts = init_gratis_start(context, runnable.configs.REF_SCHEDULE_TYPE)
+    end
+    @debug("gratis_starts : ", gratis_starts)
+
+    tso_actions = filter_tso_actions(get_tso_actions(context),
+                                    keep_limitations=runnable.configs.CONSIDER_TSOACTIONS_LIMITATIONS,
+                                    keep_impositions=runnable.configs.CONSIDER_TSOACTIONS_IMPOSITIONS)
+
+    runnable.configs.out_path = context.out_dir
+    runnable.configs.problem_name = problem_name_l
+
+    return energy_market(get_network(context),
+                        TS,
+                        get_generators_initial_state(context),
+                        get_scenarios(context, ech),
+                        get_uncertainties(context, ech),
+                        firmness,
+                        get_market_schedule(context),
+                        get_tso_schedule(context),
+                        tso_actions,
+                        gratis_starts,
+                        runnable.configs
+                        )
+end
+
+function update_market_schedule!(context::AbstractContext, ech,
+                                result::EnergyMarketModel,
+                                firmness,
+                                runnable::Union{EnergyMarket,BalanceMarket})
+    market_schedule = get_market_schedule(context)
+    market_schedule.decider_type = DeciderType(runnable)
+    market_schedule.decision_time = ech
+
+    # Production level
+    for ((gen_id, ts, s), p_injected_var) in result.limitable_model.p_injected
+        set_prod_value!(market_schedule, gen_id, ts, s, value(p_injected_var))
+    end
+    for ((gen_id, ts, s), p_injected_var) in result.imposable_model.p_injected
+        if get_power_level_firmness(firmness, gen_id, ts) == FREE
+            set_prod_value!(market_schedule, gen_id, ts, s, value(p_injected_var))
+        elseif get_power_level_firmness(firmness, gen_id, ts) == TO_DECIDE
+            set_prod_definitive_value!(market_schedule, gen_id, ts, value(p_injected_var))
+        elseif get_power_level_firmness(firmness, gen_id, ts) == DECIDED
+            @assert( ismissing(get_prod_value(market_schedule, gen_id, ts))
+                    || (value(p_injected_var) ≈ get_prod_value(market_schedule, gen_id, ts)) )
+            set_prod_definitive_value!(market_schedule, gen_id, ts, value(p_injected_var))
+        end
+    end
+
+    # Commitment
+    for ((gen_id, ts, s), b_on_var) in result.imposable_model.b_on
+        gen_state_value = parse(GeneratorState, value(b_on_var))
+        if get_commitment_firmness(firmness, gen_id, ts) == FREE
+            set_commitment_value!(market_schedule, gen_id, ts, s, gen_state_value)
+        elseif get_commitment_firmness(firmness, gen_id, ts) in [TO_DECIDE, DECIDED]
+            set_commitment_definitive_value!(market_schedule, gen_id, ts, gen_state_value)
+        end
+    end
+
+    # Capping
+    distribute_capping_by_uncertainties = runnable.configs.force_limitables
+    update_schedule_capping!(market_schedule, context, ech, result.limitable_model, distribute_capping_by_uncertainties)
+
+    # cut_conso (load-shedding)
+    update_schedule_cut_conso!(market_schedule, context, ech, result.slack_model)
+
+    return market_schedule
+end
+
