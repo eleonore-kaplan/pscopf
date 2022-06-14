39e351f8b60c6a583f1da16aba510d8d4877fd2f
Aziz Jegham <aziz.jegham@eurodecision.com>
2022-06-14 09:11:21 +0200

    ptdf checks
    Former-commit-id: f7ac7b74d58ffe15da215950d7ef8c2e3afcf14b

--- src/checkers.jl
+++ src/checkers.jl
@@ -28,7 +28,7 @@ end
 ##################################
 
 function check_ptdf(ptdf::Networks.PTDFDict, network::Network)::Bool
-    checks = true
+    checks = check_ptdf_case_entries(ptdf, network)
     for (_, ptdf_values) in ptdf
         checks &= check_ptdf(ptdf_values, network)
     end
@@ -88,6 +88,34 @@ function check_ptdf_bus_entries(ptdf::Networks.PTDFValues, network::Network)::Bo
 
     end
     return checks
+end
+
+"""
+    Prints error messages but always returns true.
+"""
+function check_ptdf_case_entries(ptdf::Networks.PTDFDict, network::Network)::Bool
+    checks = true
+
+    required_cases = Set{String}(map(Networks.get_id, Networks.get_branches(network)))
+    push!(required_cases, Networks.BASECASE)
+    ptdf_cases = Set{String}(keys(ptdf))
+
+    missing_entries = setdiff(required_cases, ptdf_cases)
+    if !isempty(missing_entries)
+        checks = false
+        msg = @sprintf("Missing case entries `%s` in ptdf", missing_entries)
+        @error(msg)
+    end
+
+    extra_entries = setdiff(ptdf_cases, required_cases)
+    if !isempty(extra_entries)
+        checks = false
+        msg = @sprintf("Extra case entries `%s` in ptdf", extra_entries)
+        @error(msg)
+    end
+
+    # return checks
+    return true
 end
 
 ####    Generator
--- test/test_wip/test_coherence_checks/test_check_ptdf.jl
+++ test/test_wip/test_coherence_checks/test_check_ptdf.jl
@@ -10,51 +10,182 @@ using DataStructures
     PSCOPF.add_new_branch!(network, "branch_1_2", 1000.)
     PSCOPF.add_new_branch!(network, "branch_1_3", 1000.)
 
-    @testset "definition_example" begin
-        ptdf = PSCOPF.Networks.PTDFValues(
-                        "branch_1_2" => SortedDict("bus_1"=>0.,
-                                                    "bus_2"=>0.,
-                                                    "bus_3"=>0.),
-                        "branch_1_3" => SortedDict("bus_1"=>0.,
-                                                    "bus_2"=>0.,
-                                                    "bus_3"=>0.)
-        )
+    @testset "ptdf_dict" begin
+        @testset "working_ptdf" begin
+            ptdf = PSCOPF.Networks.PTDFDict(
+                "BASECASE" => PSCOPF.Networks.PTDFValues(
+                                    "branch_1_2" => SortedDict("bus_1"=>0.,
+                                                                "bus_2"=>0.,
+                                                                "bus_3"=>0.),
+                                    "branch_1_3" => SortedDict("bus_1"=>0.,
+                                                                "bus_2"=>0.,
+                                                                "bus_3"=>0.)
+                ),
+                "branch_1_2" => PSCOPF.Networks.PTDFValues(
+                                    "branch_1_2" => SortedDict("bus_1"=>0.,
+                                                                "bus_2"=>0.,
+                                                                "bus_3"=>0.),
+                                    "branch_1_3" => SortedDict("bus_1"=>0.,
+                                                                "bus_2"=>0.,
+                                                                "bus_3"=>0.)
+                ),
+                "branch_1_3" => PSCOPF.Networks.PTDFValues(
+                                    "branch_1_2" => SortedDict("bus_1"=>0.,
+                                                                "bus_2"=>0.,
+                                                                "bus_3"=>0.),
+                                    "branch_1_3" => SortedDict("bus_1"=>0.,
+                                                                "bus_2"=>0.,
+                                                                "bus_3"=>0.)
+                ),
+            )
 
-        @test PSCOPF.check_ptdf(ptdf, network)
+            @test PSCOPF.check_ptdf_case_entries(ptdf, network)
+            @test PSCOPF.check_ptdf(ptdf, network)
+        end
+
+        @testset "ptdf_with_missing_n-1" begin
+            ptdf = PSCOPF.Networks.PTDFDict(
+                "BASECASE" => PSCOPF.Networks.PTDFValues(
+                                    "branch_1_2" => SortedDict("bus_1"=>0.,
+                                                                "bus_2"=>0.,
+                                                                "bus_3"=>0.),
+                                    "branch_1_3" => SortedDict("bus_1"=>0.,
+                                                                "bus_2"=>0.,
+                                                                "bus_3"=>0.)
+                ),
+                "branch_1_2" => PSCOPF.Networks.PTDFValues(
+                                    "branch_1_2" => SortedDict("bus_1"=>0.,
+                                                                "bus_2"=>0.,
+                                                                "bus_3"=>0.),
+                                    "branch_1_3" => SortedDict("bus_1"=>0.,
+                                                                "bus_2"=>0.,
+                                                                "bus_3"=>0.)
+                ),
+            )
+
+            @test_broken !PSCOPF.check_ptdf_case_entries(ptdf, network)
+            @test_broken !PSCOPF.check_ptdf(ptdf, network)
+        end
+
+        @testset "ptdf_with_missing_branch_values" begin
+            ptdf = PSCOPF.Networks.PTDFDict(
+                "BASECASE" => PSCOPF.Networks.PTDFValues(
+                                    "branch_1_2" => SortedDict("bus_1"=>0.,
+                                                                "bus_2"=>0.,
+                                                                "bus_3"=>0.),
+                                    # "branch_1_3" => SortedDict("bus_1"=>0.,
+                                    #                             "bus_2"=>0.,
+                                    #                             "bus_3"=>0.)
+                ),
+                "branch_1_2" => PSCOPF.Networks.PTDFValues(
+                                    "branch_1_2" => SortedDict("bus_1"=>0.,
+                                                                "bus_2"=>0.,
+                                                                "bus_3"=>0.),
+                                    "branch_1_3" => SortedDict("bus_1"=>0.,
+                                                                "bus_2"=>0.,
+                                                                "bus_3"=>0.)
+                ),
+                "branch_1_3" => PSCOPF.Networks.PTDFValues(
+                                    "branch_1_2" => SortedDict("bus_1"=>0.,
+                                                                "bus_2"=>0.,
+                                                                "bus_3"=>0.),
+                                    "branch_1_3" => SortedDict("bus_1"=>0.,
+                                                                "bus_2"=>0.,
+                                                                "bus_3"=>0.)
+                ),
+            )
+
+            @test PSCOPF.check_ptdf_case_entries(ptdf, network)
+            @test !PSCOPF.check_ptdf(ptdf, network)
+        end
+
+        @testset "ptdf_with_missing_bus_value" begin
+            ptdf = PSCOPF.Networks.PTDFDict(
+                "BASECASE" => PSCOPF.Networks.PTDFValues(
+                                    "branch_1_2" => SortedDict("bus_1"=>0.,
+                                                                "bus_2"=>0.,
+                                                                "bus_3"=>0.),
+                                    "branch_1_3" => SortedDict("bus_1"=>0.,
+                                                                "bus_2"=>0.,
+                                                                "bus_3"=>0.)
+                ),
+                "branch_1_2" => PSCOPF.Networks.PTDFValues(
+                                    "branch_1_2" => SortedDict("bus_1"=>0.,
+                                                                "bus_2"=>0.,
+                                                                "bus_3"=>0.),
+                                    "branch_1_3" => SortedDict("bus_1"=>0.,
+                                                                "bus_2"=>0.,
+                                                                "bus_3"=>0.)
+                ),
+                "branch_1_3" => PSCOPF.Networks.PTDFValues(
+                                    "branch_1_2" => SortedDict("bus_1"=>0.,
+                                                                "bus_2"=>0.,
+                                                                "bus_3"=>0.),
+                                    "branch_1_3" => SortedDict("bus_1"=>0.,
+                                                                # "bus_2"=>0.,
+                                                                "bus_3"=>0.)
+                ),
+            )
+
+            @test PSCOPF.check_ptdf_case_entries(ptdf, network)
+            @test !PSCOPF.check_ptdf(ptdf, network)
+        end
+
     end
 
-    @testset "missing_branch_1_3_entry" begin
-        ptdf = SortedDict{String,SortedDict{String, Float64}}(
-                        "branch_1_2" => SortedDict("bus_1"=>0.,
-                                                    "bus_2"=>0.,
-                                                    "bus_3"=>0.)
-        )
-
-        @test !PSCOPF.check_ptdf(ptdf, network)
-    end
-
-    @testset "missing_bus_2_in_branch_1_2" begin
-        ptdf = SortedDict{String,SortedDict{String, Float64}}(
-                        "branch_1_2" => SortedDict("bus_1"=>0.,
-                                                    "bus_3"=>0.),
-                        "branch_1_3" => SortedDict("bus_1"=>0.,
-                                                    "bus_2"=>0.,
-                                                    "bus_3"=>0.)
-        )
-
-        @test !PSCOPF.check_ptdf(ptdf, network)
-    end
-
-    @testset "missing_value_for_branch_1_2_bus_3" begin
-        ptdf = SortedDict(
+    @testset "check_ptdf_values" begin
+        @testset "definition_example" begin
+            ptdf = PSCOPF.Networks.PTDFValues(
                             "branch_1_2" => SortedDict("bus_1"=>0.,
                                                         "bus_2"=>0.,
-                                                        "bus_3"=>missing),
+                                                        "bus_3"=>0.),
                             "branch_1_3" => SortedDict("bus_1"=>0.,
                                                         "bus_2"=>0.,
                                                         "bus_3"=>0.)
-                        )
-        @test_throws MethodError PSCOPF.check_ptdf(ptdf, network)
+            )
+
+            @test PSCOPF.check_ptdf_branch_entries(ptdf, network)
+            @test PSCOPF.check_ptdf_bus_entries(ptdf, network)
+            @test PSCOPF.check_ptdf(ptdf, network)
+        end
+
+        @testset "missing_branch_1_3_entry" begin
+            ptdf = PSCOPF.Networks.PTDFValues(
+                            "branch_1_2" => SortedDict("bus_1"=>0.,
+                                                        "bus_2"=>0.,
+                                                        "bus_3"=>0.)
+            )
+
+            @test !PSCOPF.check_ptdf_branch_entries(ptdf, network)
+            @test PSCOPF.check_ptdf_bus_entries(ptdf, network)
+            @test !PSCOPF.check_ptdf(ptdf, network)
+        end
+
+        @testset "missing_bus_2_in_branch_1_2" begin
+            ptdf = PSCOPF.Networks.PTDFValues(
+                            "branch_1_2" => SortedDict("bus_1"=>0.,
+                                                        "bus_3"=>0.),
+                            "branch_1_3" => SortedDict("bus_1"=>0.,
+                                                        "bus_2"=>0.,
+                                                        "bus_3"=>0.)
+            )
+
+            @test PSCOPF.check_ptdf_branch_entries(ptdf, network)
+            @test !PSCOPF.check_ptdf_bus_entries(ptdf, network)
+            @test !PSCOPF.check_ptdf(ptdf, network)
+        end
+
+        @testset "missing_value_for_branch_1_2_bus_3" begin
+            ptdf = SortedDict(
+                                "branch_1_2" => SortedDict("bus_1"=>0.,
+                                                            "bus_2"=>0.,
+                                                            "bus_3"=>missing),
+                                "branch_1_3" => SortedDict("bus_1"=>0.,
+                                                            "bus_2"=>0.,
+                                                            "bus_3"=>0.)
+                            )
+            @test_throws MethodError PSCOPF.check_ptdf(ptdf, network)
+        end
     end
 
 end
